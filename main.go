f19a4e988d4afc4c3338bdcaf73f6ae7fefa332c
Aleksy37 <aleksyveillette@gmail.com>
2026-08-31 16:17:03 -0400

    added statefulness for metric tracking and endpoint to check/reset metrics

--- main.go
+++ main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
-
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	port := "8080"
 	filepathRoot := "."
+
+	apiCfg := &apiConfig{}
 	
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetricCount)
+	mux.HandleFunc("/reset", apiCfg.handlerMetricReset)
+
 	
 	svr := &http.Server{
 		Addr: ":" + port,
@@ -24,8 +33,19 @@ func main() {
 	log.Fatal(svr.ListenAndServe())
 }
 
-func handlerReadiness(w http.ResponseWriter, r *http.Request)  {
-			w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-			w.WriteHeader(200)
-			w.Write([]byte(http.StatusText(http.StatusOK)))
-		}
+
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+
+func (cfg *apiConfig) handlerMetricCount(w http.ResponseWriter, r *http.Request)  {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+	}
+
